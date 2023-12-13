f325d69b592c992ee7112a51ba6e71d01fc4fa60
诸葛子房 <xiaoqiu2017wy@163.com>
2023-12-13 17:06:25 +0800

    fix:add log

--- streampark-console/streampark-console-service/src/main/java/org/apache/streampark/console/core/service/impl/MessageServiceImpl.java
+++ streampark-console/streampark-console-service/src/main/java/org/apache/streampark/console/core/service/impl/MessageServiceImpl.java
@@ -49,6 +49,7 @@ public class MessageServiceImpl extends ServiceImpl<MessageMapper, Message>
   @Override
   public IPage<Message> getUnReadPage(NoticeTypeEnum noticeTypeEnum, RestRequest request) {
     int type = noticeTypeEnum.get();
+    log.info("type:{}", type);
     Page<Message> page = new MybatisPager<Message>().getDefaultPage(request);
     LambdaQueryWrapper<Message> queryWrapper =
         new LambdaQueryWrapper<Message>()
